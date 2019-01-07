bc74b4ab60d1010d4e871c6c04422b97a5d94ddb
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2019-01-07 13:46:16 -0800

    Startup VPN + services

--- root/startup.sh
+++ root/startup.sh
@@ -1,24 +1,41 @@
 #! /bin/sh
 
-INTERNAL_INTERFACE=eth0
+HOME_INTERFACE=eth0
+INTERNAL_INTERFACE=eth1
 EXTERNAL_INTERFACE=tun0
+PROTO=udp
 
+if [ "${PORT}" = "" ]; then
+  echo "No PORT set"
+  exit 1
+fi
 
-if [ ! -e /etc/openvpn/pki ]; then
-  EASYRSA=/usr/share/easy-rsa/easyrsa
+IP=$(ip addr show dev ${HOME_INTERFACE} | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | head -1)
+
+# Block all in and out traffic (except openvpn) on the ${HOME_INTERFACE}
+iptables -A INPUT -i ${HOME_INTERFACE} -p ${PROTO} --dport ${PORT} -j ACCEPT
+iptables -A OUTPUT -i ${HOME_INTERFACE} -p ${PROTO} --sport ${PORT} -j ACCEPT
+iptables -A INPUT -i ${HOME_INTERFACE} -j DROP
+iptables -A OUTPUT -i ${HOME_INTERFACE} -j DROP
+route del default
+
+if [ ! -e /etc/openvpn/pki/crl.pem ]; then
+  PATH=$PATH:/usr/share/easy-rsa
   cd /etc/openvpn
-  ${EASYRSA} init-pki
-  EASYRSA_BATCH=1 ${EASYRSA} build-ca nopass
-  ${EASYRSA} gen-dh
+  rm -rf pki ta.key
+  easyrsa init-pki
+  EASYRSA_BATCH=1 easyrsa build-ca nopass
+  easyrsa gen-dh
   openvpn --genkey --secret ta.key
-  ${EASYRSA} build-server-full minke nopass
-  ${EASYRSA} gen-crl
+  easyrsa build-server-full minke nopass
+  easyrsa gen-crl
   cd /
 fi
 
-cat > /etc/openvpn.conf <<__EOF__
-port 1194
-proto udp
+echo "port ${PORT}" > /etc/openvpn.conf
+echo "local ${IP}" >> /etc/openvpn.conf
+echo "proto ${PROTO}" >> /etc/openvpn.conf
+cat >> /etc/openvpn.conf <<__EOF__
 dev tun
 ca /etc/openvpn/pki/ca.crt
 cert /etc/openvpn/pki/issued/minke.crt
@@ -34,7 +51,7 @@ auth SHA256
 ncp-ciphers AES-256-GCM
 keepalive 10 120
 __EOF__
-/usr/sbin/openvpn --config /etc/openvpn.conf --daemon
+openvpn --config /etc/openvpn.conf --daemon
 
 # NAT firewall (${INTERNAL_INTERFACE} -> ${EXTERNAL_INTERFACE})
 iptables -t nat -A POSTROUTING -o ${EXTERNAL_INTERFACE} -j MASQUERADE
@@ -52,7 +69,7 @@ min_lifetime=120
 max_lifetime=86400
 secure_mode=no
 notify_interval=60
-allow 0-65535 172.17.0.0/16 0-65535
+allow 0-65535 172.0.0.0/8 0-65535
 deny 0-65535 0.0.0.0/0 0-65535
 __EOF__
 miniupnpd -f /etc/miniupnpd.conf
