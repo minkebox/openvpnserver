c48bdc73ad8a94e3f7721d4eaba31fd85e63a907
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2019-01-09 11:31:30 -0800

    Allow mDNS traffic

--- root/startup.sh
+++ root/startup.sh
@@ -123,6 +123,9 @@ openvpn --config /etc/openvpn/minke-server.ovpn --daemon
 # UPnP traffic handled locally
 iptables -t nat -A PREROUTING -p udp --dport 1900 -j ACCEPT -i ${INTERNAL_INTERFACE}
 iptables -t nat -A PREROUTING -p tcp --dport 1900 -j ACCEPT -i ${INTERNAL_INTERFACE}
+# mDNS traffic handled locally
+iptables -t nat -A PREROUTING -p udp --dport 5353 -j ACCEPT -i ${INTERNAL_INTERFACE}
+iptables -t nat -A PREROUTING -p udp --dport 5353 -j ACCEPT -i ${EXTERNAL_INTERFACE}
 # Everything else we forward
 iptables -t nat -A PREROUTING  -j DNAT --to-destination ${EXTERNAL_REMOTE_IP} -i ${INTERNAL_INTERFACE}
 iptables -t nat -A POSTROUTING -j MASQUERADE -o ${EXTERNAL_INTERFACE}
