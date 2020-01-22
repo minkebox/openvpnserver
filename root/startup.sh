9b7fed1f0ee9e93ee8808c6ab105864adc3c544a
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2020-01-22 14:36:57 -0800

    Support tun and tap vpns

--- root/startup.sh
+++ root/startup.sh
@@ -2,20 +2,22 @@
 
 HOME_INTERFACE=${__HOME_INTERFACE}
 PRIVATE_INTERFACE=${__PRIVATE_INTERFACE}
-EXTERNAL_INTERFACE_TYPE=tap
-EXTERNAL_INTERFACE=${EXTERNAL_INTERFACE_TYPE}0
 PROTO=udp
 DDNS_DOMAIN=minkebox.net
 PRIVATE_HOSTNAME=${__GLOBALID}
 
 ROOT=/etc/openvpn
-SERVER_CONFIG=${ROOT}/minke-server.ovpn
-CLIENT_CONFIG=${ROOT}/minke-client.ovpn
-ORIGINAL_CLIENT_CONFIG=/etc/config.ovpn
+SERVER_CONFIG_TAP=${ROOT}/minke-server.ovpn
+SERVER_CONFIG_TUN=${ROOT}/minke-server-tun.ovpn
+CLIENT_CONFIG_TAP=${ROOT}/minke-client.ovpn
+CLIENT_CONFIG_TUN=${ROOT}/minke-client-tun.ovpn
+ORIGINAL_CLIENT_CONFIG_TAP=/etc/config.ovpn
+ORIGINAL_CLIENT_CONFIG_TUN=/etc/config-alt.ovpn
 PORTRANGE_START=41310
 PORTRANGE_LEN=256
 TTL=600 # 10 minutes
 TTL2=300 # TTL/2
+SERVER_NETWORK_TUN=10.224.76.0
 SERVER_BRIDGE=
 
 HOME_IP=$(ip addr show dev ${HOME_INTERFACE} | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | head -1)
@@ -27,6 +29,7 @@ if [ "${PRIVATE_INTERFACE}" != "" ]; then
 else
   BRIDGE_IP=${HOME_IP}
   BRIDGE_INTERFACE=${HOME_INTERFACE}
+  BRIDGE_IP_ROOT=$(echo ${BRIDGE_IP} | sed "s/^\(\d\+.\d\+.\d\+\).*$/\1/")
 fi
 
 
@@ -51,14 +54,17 @@ if [ ! -e ${ROOT}/config-done ]; then
   cd /
 fi
 
-if [ ! -e ${CLIENT_CONFIG} ]; then
+if [ ! -e ${CLIENT_CONFIG_TAP} ]; then
 
   # Select an unused port at random from within our standard range avoiding any we see as in use
   active_ports=$(upnpc -u ${__UPNPURL} -m ${HOME_INTERFACE} -L | grep "^ *\d\? UDP\|TCP .*$" | sed "s/^.*:\(\d*\).*$/\1/")
   while true ; do
-    PORT=$((${PORTRANGE_START} + RANDOM % ${PORTRANGE_LEN}))
-    if ! $(echo $active_ports | grep -q ${PORT}); then
-      break;
+    PORT_TAP=$((${PORTRANGE_START} + RANDOM % ${PORTRANGE_LEN}))
+    PORT_TUN=$((${PORT_TAP} + 1))
+    if ! $(echo $active_ports | grep -q ${PORT_TAP}); then
+      if ! $(echo $active_ports | grep -q ${PORT_TUN}); then
+        break;
+      fi
     fi
   done
 
@@ -68,11 +74,11 @@ if [ ! -e ${CLIENT_CONFIG} ]; then
   cd /
   echo "client
 nobind
-dev ${EXTERNAL_INTERFACE_TYPE}
+dev tap
 persist-key
 persist-tun
 remote-cert-tls server
-remote ${PRIVATE_HOSTNAME}.${DDNS_DOMAIN} ${PORT} ${PROTO}
+remote ${PRIVATE_HOSTNAME}.${DDNS_DOMAIN} ${PORT_TAP} ${PROTO}
 key-direction 1
 cipher AES-256-CBC
 auth SHA256
@@ -89,20 +95,49 @@ $(cat ${ROOT}/pki/ca.crt)
 </ca>
 <tls-auth>
 $(cat ${ROOT}/pki/ta.key)
-</tls-auth>" > ${CLIENT_CONFIG}
+</tls-auth>" > ${CLIENT_CONFIG_TAP}
 fi
 
-# Make it retrievable
-cat ${CLIENT_CONFIG} > ${ORIGINAL_CLIENT_CONFIG}
-
 # Extract port from client config
-PORT=$(grep "^remote " ${CLIENT_CONFIG} | sed "s/^remote .* \(\d\+\) .*/\1/")
+PORT_TAP=$(grep "^remote " ${CLIENT_CONFIG_TAP} | sed "s/^remote .* \(\d\+\) .*/\1/")
+PORT_TUN=$((${PORT_TAP} + 1))
+
+if [ ! -e ${CLIENT_CONFIG_TUN} ]; then
+  echo "client
+nobind
+dev tun
+persist-key
+persist-tun
+remote-cert-tls server
+remote ${PRIVATE_HOSTNAME}.${DDNS_DOMAIN} ${PORT_TUN} ${PROTO}
+key-direction 1
+cipher AES-256-CBC
+auth SHA256
+ncp-ciphers AES-256-GCM
+push-peer-info
+<key>
+$(cat ${ROOT}/pki/private/minke-client.key)
+</key>
+<cert>
+$(cat ${ROOT}/pki/issued/minke-client.crt)
+</cert>
+<ca>
+$(cat ${ROOT}/pki/ca.crt)
+</ca>
+<tls-auth>
+$(cat ${ROOT}/pki/ta.key)
+</tls-auth>" > ${CLIENT_CONFIG_TUN}
+fi
+
+# Make them retrievable
+cat ${CLIENT_CONFIG_TAP} > ${ORIGINAL_CLIENT_CONFIG_TAP}
+cat ${CLIENT_CONFIG_TUN} > ${ORIGINAL_CLIENT_CONFIG_TUN}
 
 # Always generate the server config (in case the network changed so SERVER_BRIDGE has changed)
 echo "server-bridge ${SERVER_BRIDGE}
-port ${PORT}
+port ${PORT_TAP}
 proto ${PROTO}
-dev ${EXTERNAL_INTERFACE}
+dev tap0
 key-direction 0
 <ca>
 $(cat ${ROOT}/pki/ca.crt)
@@ -127,32 +162,72 @@ ncp-ciphers AES-256-GCM
 duplicate-cn
 client-to-client
 explicit-exit-notify 1
-keepalive 10 60" > ${SERVER_CONFIG}
+keepalive 10 60" > ${SERVER_CONFIG_TAP}
 
-trap "upnpc -u ${__UPNPURL} -m br0 -d ${PORT} ${PROTO}; killall sleep openvpn; exit" TERM INT
+echo "server ${SERVER_NETWORK_TUN} 255.255.255.0
+port ${PORT_TUN}
+proto ${PROTO}
+dev tun0
+key-direction 0
+<ca>
+$(cat ${ROOT}/pki/ca.crt)
+</ca>
+<cert>
+$(cat ${ROOT}/pki/issued/minke-simple-vpn.crt)
+</cert>
+<key>
+$(cat ${ROOT}/pki/private/minke-simple-vpn.key)
+</key>
+<tls-auth>
+$(cat ${ROOT}/pki/ta.key)
+</tls-auth>
+<dh>
+$(cat ${ROOT}/pki/dh.pem)
+</dh>
+persist-key
+persist-tun
+push \"route ${BRIDGE_IP_ROOT}.0 255.255.255.0\"
+push \"dhcp-option DNS ${__DNSSERVER}\"
+cipher AES-256-CBC
+auth SHA256
+ncp-ciphers AES-256-GCM
+duplicate-cn
+client-to-client
+explicit-exit-notify 1
+keepalive 10 60" > ${SERVER_CONFIG_TUN}
 
-# Bridge
-openvpn --mktun --dev ${EXTERNAL_INTERFACE}
+trap "upnpc -u ${__UPNPURL} -m br0 -d ${PORT_TAP} ${PROTO}; upnpc -u ${__UPNPURL} -m br0 -d ${PORT_TUN} ${PROTO}; killall sleep openvpn; exit" TERM INT
 
+# Bridge the TAP vpn
+openvpn --mktun --dev tap0
 brctl addbr br0
 ifconfig ${BRIDGE_INTERFACE} 0.0.0.0 up
-ifconfig ${EXTERNAL_INTERFACE} 0.0.0.0 up
+ifconfig tap0 0.0.0.0 up
 ifconfig br0 ${BRIDGE_IP} netmask 255.255.255.0 up
 # Bridge inherits "main" network mac address
 /sbin/ip link set br0 address $(cat /sys/class/net/${BRIDGE_INTERFACE}/address)
 brctl addif br0 ${BRIDGE_INTERFACE}
-brctl addif br0 ${EXTERNAL_INTERFACE}
+brctl addif br0 tap0
 route add default gw ${__GATEWAY}
 
-openvpn --daemon --config ${SERVER_CONFIG}
+# Masquarade the TUN vpn
+#iptables -I FORWARD -i tun0 -o br0 -m conntrack --ctstate NEW -j ACCEPT
+#iptables -I FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+iptables -t nat -I POSTROUTING -o br0 -s ${SERVER_NETWORK_TUN}/24 -j MASQUERADE
+
+openvpn --daemon --config ${SERVER_CONFIG_TAP}
+openvpn --daemon --config ${SERVER_CONFIG_TUN}
 
 # Open the NAT
 sleep 1 &
 while wait "$!"; do
-  upnpc -u ${__UPNPURL} -e ${HOSTNAME} -a ${HOME_IP} ${PORT} ${PORT} ${PROTO} ${TTL}
+  upnpc -u ${__UPNPURL} -e ${HOSTNAME}_tap -a ${HOME_IP} ${PORT_TAP} ${PORT_TAP} ${PROTO} ${TTL}
+  upnpc -u ${__UPNPURL} -e ${HOSTNAME}_tun -a ${HOME_IP} ${PORT_TUN} ${PORT_TUN} ${PROTO} ${TTL}
   if [ "${__HOSTIP6}" != "" ]; then
-    upnpc -u ${__UPNPURL} -e ${HOSTNAME}_6 -6 -A "" 0 ${__HOSTIP6} ${PORT} ${PROTO} ${TTL}
+    upnpc -u ${__UPNPURL} -e ${HOSTNAME}_tap6 -6 -A "" 0 ${__HOSTIP6} ${PORT_TAP} ${PROTO} ${TTL}
+    upnpc -u ${__UPNPURL} -e ${HOSTNAME}_tun6 -6 -A "" 0 ${__HOSTIP6} ${PORT_TUN} ${PROTO} ${TTL}
   fi
   sleep ${TTL2} &
 done
-upnpc -u ${__UPNPURL} -d ${PORT} ${PROTO}
+upnpc -u ${__UPNPURL} -d ${PORT_TAP} ${PROTO}
+upnpc -u ${__UPNPURL} -d ${PORT_TUN} ${PROTO}
